1037d9c865edd58e18fd1a35a48232ffb563319b
Bruno Paulino <bruno@woombikes.com>
2018-05-29 10:55:44 +0200

    update endpoint

--- src/util/Http.js
+++ src/util/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Http = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://kanban-api-rails.herokuapp.com',
   headers: { 'Access-Control-Allow-Origin': '*' }
 });
 
